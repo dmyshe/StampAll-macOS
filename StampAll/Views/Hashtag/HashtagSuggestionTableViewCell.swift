b96ac10cd36dc4fabb3e399da9ad772ae10d9d74
dmyshe <94231197+dmyshe@users.noreply.github.com>
2022-08-11 23:39:26 +0300

    Add HashtagSuggestionTableViewCell

---
+++ StampAll/Views/Hashtag/HashtagSuggestionTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  HashtagSuggestionTableViewCell.swift
+//  StampAll
+//
+//  Created by Дмитро  on 11.08.2022.
+//
+
+import AppKit
+
+class HashtagSuggestionTableViewCell: NSTableCellView {
+    @IBOutlet  weak var hastagNameLabel: NSTextField!
+}
